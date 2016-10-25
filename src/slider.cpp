c5798cbf68f3324c69aadef1604de0fdc539b6f0
Ian Oltuszyk <ian.oltu@gmail.com>
2016-10-24 18:41:03 -0600

    Seperated test from master

--- src/slider.cpp
+++ src/slider.cpp
@@ -1,18 +1,4 @@
-extern "C"
-{
-#include <luaconf.h>
-#include <lua.h>
-#include <lualib.h>
-#include <lauxlib.h>
-#include <luajit.h>
-}
-
 int main(int argc, char** argv)
 {
-	lua_State* L = lua_open();
-	luaL_openlibs(L);
-	luaL_dofile(L, "test/example.lua");
-	lua_close(L);
-
 	return 0;
 }
