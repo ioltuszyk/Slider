456e2c5eb193e4926a911157bd8b3bfe543c721a
Ian Oltuszyk <ian.oltu@gmail.com>
2016-11-16 20:21:41 -0700

    Bin Added

---
+++ bin/Lua/instructions.lua
@@ -0,0 +1,17 @@
+﻿alert("Instructions", [[
+Use the arrow keys to navigate the menus
+
+You will be prompted to select the rulesets for:
+    Spawning
+    Movement
+
+You will then be able to select whether to:
+    Start a New Game
+    Exit
+
+Controls when playing a game:
+    Space = Start/Stop Solver
+    Arrow Keys = Manual Input (when stopped)
+    O = Output Current Statistics
+    Q = Exit Game
+]])
