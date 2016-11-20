c6d5d1da220654c59892a3aa8d6c1cc626d4d90d
Victor Frunza <vfrunza@ualberta.ca>
2016-11-20 00:50:05 -0700

    Monotonic Heuristic Completed

--- src/Lua/heuristic.lua
+++ src/Lua/heuristic.lua
@@ -11,74 +11,95 @@ function empty_tiles(state)
 end
 
 state = {
-	Tiles = {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1}
+	Tiles = {1, 1, 0, 0, 7, 0, 1, 0, 5, 6, 1, 0, 0, 2, 0, 3}
 }
 
 time.reset()
 for n=1, 200000 do
 	h = empty_tiles(state)
 end
-print("Empty Tiles: " .. time.getElapsed())
+print("Empty Tiles: " ..h)
+print("Time: " .. time.getElapsed())
 
-print(h)
-
-function merges(state)
-	--not optimized for the position its moving into, ill leave it like this until i can test it properly.
-	local cur = 1
-	local merge = 0
-	
-	while cur < 16 do
-		print("cur: ".. cur)
-		if (state.Tiles[cur] == state.Tiles[cur + 1]) then
-			merge = merge + 1
-			if (cur == 3) then
-				cur = 5
-			elseif (cur == 7) then
-				cur = 9
-			elseif (cur == 11) then
-				cur = 13
-			else
-				cur = cur + 2
-			end
-		else
-			cur = cur + 1
-			print("hello")
-		end
+function manhatten_dist(ideal, actual)
+	if (ideal == actual) then
+		return 0
 	end
-	--cur = 1
-	--[[while true do
-		if (cur >= 16) do
-			break
-		end
+	if(actual == 4 or actual == 8 or actual == 12 or actual == 16) then
+		ax = 4
+	else
+		ax = actual % 4
+	end
+	ay = math.ceil(actual/4)
 
-		if (state.Tiles[cur] == state.Tiles[cur + 4]) then
-			merge = merge + 1
-			cur = cur + 8
+	if(ideal == 4 or ideal == 8 or ideal == 12 or ideal == 16) then
+		ix = 4
+	else
+		ix = ideal % 4
+	end
+	iy = math.ceil(ideal/4)
 
-			if (cur >= 13) then
-				cur = 2
-			end
-			if (cur >= 14) then
-				cur = 3
-			end
-			if (cur >= 15) then
-				cur = 4
-			end
-		else
-			cur = cur + 1
-		end
-	end]]--
-	return merge
+	--print("ideal= " .. ideal .. " actual= ".. actual)
+	--print("ax= ".. ax .." ay= ".. ay .." ix= ".. ix .." iy= ".. iy .. " Dist= ".. math.abs(ax - ix) + math.abs(ay - iy))
+
+	return math.abs(ax - ix) + math.abs(ay - iy)
 end
 
-state = {
-	Tiles = {1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1}
-}
+function monotonicity(state)
+	local tile_dic = {}
+	for i=1, 16 do
+		table.insert(tile_dic, {state.Tiles[i], i, 0, false}) --value, index, mat dist, checked
+	end
 
+	--get largest tile value
+	local ideal = 1
+	local checked = 1
+	local index = 1
+	local max = 0
+
+	while checked < 17 do
+		for i=1, 16 do
+			if (tile_dic[i][1] > max and tile_dic[i][4] == false) then
+				max = tile_dic[i][1]
+				index = tile_dic[i][2]
+			end
+		end
+		if (max == 0) then
+			break
+		end
+		tile_dic[index][3] = manhatten_dist(ideal, index)
+		tile_dic[index][4] = true
+
+		--Updating values
+		if (1 <= ideal and ideal < 4) then
+			ideal = ideal + 1
+		elseif (ideal == 4) then
+			ideal = 8
+		elseif (5 < ideal and ideal <= 8) then
+			ideal = ideal - 1
+		elseif (ideal == 5) then
+			ideal = 9
+		elseif (9 <= ideal and ideal < 12) then
+			ideal = ideal + 1
+		elseif (ideal == 12) then
+			ideal = 16
+		elseif (13 < ideal and ideal <= 16) then
+			ideal = ideal - 1
+		end
+		max = 0
+		checked = checked + 1
+		index = 1
+
+	end
+	h = 0
+	for i=1, 16 do
+		h = h + (tile_dic[i][3] * tile_dic[i][1])
+	end
+	return h
+end
 time.reset()
---for n=1, 20 do
-h = merges(state)
---end
-print("Merges: " .. time.getElapsed())
-
-print(h)
+for n=1, 200000 do
+	h = monotonicity(state)
+end
+print("Monotonicity: "..h)
+print("Time: " .. time.getElapsed())
