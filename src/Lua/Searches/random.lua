a36b6ff5ea6a9035ecaf758705b4c40011a9ada0
Victor Frunza <vfrunza@ualberta.ca>
2016-11-21 23:10:57 -0700

    Run random only once

--- src/Lua/Searches/random.lua
+++ src/Lua/Searches/random.lua
@@ -1,31 +1,30 @@
-for i=1, 32 do
-	currentState = State.new(nil)
-	currentState.Tiles = {
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0,
-		0, 0, 0, 0
-	}
-	wait(3)
-	math.randomseed(os.time())
-	currentState.Tiles[math.random(#currentState.Tiles)] = math.random(2)*2
 
-	time.reset()
-	while (true) do
-		local decisions = {}
-		currentState:Branch()
-		for k, v in pairs(currentState.Tree) do
-			table.insert(decisions, k)
-		end
-		if (#decisions~=0) then
-			local index = math.random(#decisions)
-			currentState.Tree[decisions[index]]:Spawn()
-			currentState = currentState.Tree[decisions[index]].Tree[math.random(#currentState.Tree[decisions[index]].Tree)]
-		else
-			--currentState:Print()
-			break
-		end
+currentState = State.new(nil)
+currentState.Tiles = {
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0,
+	0, 0, 0, 0
+}
+wait(1)
+math.randomseed(os.time())
+currentState.Tiles[math.random(#currentState.Tiles)] = math.random(2)*2
+
+time.reset()
+while (true) do
+	local decisions = {}
+	currentState:Branch()
+	for k, v in pairs(currentState.Tree) do
+		table.insert(decisions, k)
 	end
-	print("Score: "..Heuristics.Score(currentState))
-	--print("Time: " .. time.getElapsed())
+	if (#decisions~=0) then
+		local index = math.random(#decisions)
+		currentState.Tree[decisions[index]]:Spawn()
+		currentState = currentState.Tree[decisions[index]].Tree[math.random(#currentState.Tree[decisions[index]].Tree)]
+	else
+		currentState:Print()
+		break
+	end
 end
+print("Score: "..Heuristics.Score(currentState))
+print("Time: " .. time.getElapsed())
